ea2fd3236537631fe6a09f0a87871d84b821d351
Daniel Vidali Fryer <20966023+frycast@users.noreply.github.com>
2019-04-29 01:46:32 +1000

    improve test coverage

--- tests/testthat/test_data_import.R
+++ tests/testthat/test_data_import.R
@@ -42,6 +42,25 @@ testthat::test_that( "load tables from sensing", {
   }
 })
 
+testthat::test_that( "load tables from sensing with missing csv_nrows", {
+  loc <- "testdata/SL_testdata"
+  studs_list <- list()
+  for (i in 1:length(menu_data$sensing)) {
+    studs_list[[i]] <- load_SL_tibble(
+      schema = "sensing", table = studentlife:::menu_data$sensing[i],
+      location = loc)
+    testthat::expect_true(nrow(studs_list[[!!i]]) > 0)
+    testthat::expect_true(
+      "timestamp" %in% names(studs_list[[!!i]])
+      || "start_timestamp" %in% names(studs_list[[!!i]]))
+    testthat::expect_true("uid" %in% names(studs_list[[!!i]]))
+    testthat::expect_true(
+      "timestamp_SL_tbl" %in% class(studs_list[[!!i]])
+      || "interval_SL_tbl" %in% class(studs_list[[!!i]]))
+    testthat::expect_s3_class(studs_list[[!!i]], "SL_tbl")
+  }
+})
+
 testthat::test_that( "load tables from EMA with vars = timestamp", {
   loc <- "testdata/SL_testdata"
   studs_list <- list()
@@ -78,6 +97,19 @@ testthat::test_that( "load tables from survey", {
     studs_list[[i]] <- load_SL_tibble(
       schema = "survey", table = studentlife:::menu_data$survey[i],
       location = loc, csv_nrows = 1)
+    testthat::expect_true(nrow(studs_list[[!!i]]) > 0)
+    testthat::expect_s3_class(studs_list[[!!i]], "SL_tbl")
+  }
+})
+
+
+testthat::test_that( "load tables from survey with vars = uid and csv_nrows missing", {
+  loc <- "testdata/SL_testdata"
+  studs_list <- list()
+  for (i in 1:length(menu_data$survey)) {
+    studs_list[[i]] <- load_SL_tibble(
+      schema = "survey", table = studentlife:::menu_data$survey[i],
+      location = loc, vars = "uid")
     testthat::expect_true(nrow(studs_list[[!!i]]) > 0)
     testthat::expect_s3_class(studs_list[[!!i]], "SL_tbl")
   }
